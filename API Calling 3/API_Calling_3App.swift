cb4cd9f70059eb0f57ecdfb6926f3cae11d8489d
Lena Rotteveel <lenarotteveel@gmail.com>
2021-03-31 11:07:21 -0500

    Initial Commit

---
+++ API Calling 3/API_Calling_3App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_3App.swift
+//  API Calling 3
+//
+//  Created by Lena Rotteveel on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Calling 3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API Calling 3
+//
+//  Created by Lena Rotteveel on 3/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
